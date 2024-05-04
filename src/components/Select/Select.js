5c7e482eb787af24d49763a0a603d9df5e157629
Henrik Sundman <henriksundman@yahoo.se>
2024-05-04 13:39:12 +0200

    complete progressbar, first attempt Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -6,13 +6,47 @@ import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+	const displayedValue = getDisplayedValue(value, children);
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
+	const styles = {
+		width: [...displayedValue].length + 'em',
+	};
+
+	console.log(styles);
+	return (
+		<Wrapper style={styles}>
+			<SelectBox value={value} onChange={onChange} style={styles}>
+				{children}
+			</SelectBox>
+			<IconWrapper>
+				<Icon id="chevron-down" size={24} />
+			</IconWrapper>
+		</Wrapper>
+	);
 };
+
+const Wrapper = styled.div`
+	position: relative;
+`;
+
+const SelectBox = styled.select`
+	appearance: none;
+	width: var(--current-width);
+	font-size: 16px;
+	padding: 12px 52px 12px 16px;
+	background-color: ${COLORS.transparentGray15};
+	border: none;
+	border-radius: 8px;
+	color: ;
+	cursor: pointer;
+`;
+
+const IconWrapper = styled.div`
+	z-index: -1;
+	position: absolute;
+	right: 10px;
+	top: 10px;
+	cursor: pointer;
+`;
 
 export default Select;
