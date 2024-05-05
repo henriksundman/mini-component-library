a069c69aac12259d2fd7d6014c1464c1da8337a8
Henrik Sundman <henriksundman@yahoo.se>
2024-05-05 19:45:47 +0200

    iconinput first solution

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -7,18 +7,19 @@ import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
 const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+	const iconStyles = {};
+	iconStyles.display = 'inline-block';
+	iconStyles.marginBottom = size === 'small' ? '-5.2px' : '-6.8px';
+	iconStyles.borderBottom =
+		size === 'small' ? '1px solid black' : '2px solid black';
+
+	const iconSize = size === 'small' ? 16 : 24;
+
 	return (
 		<Wrapper>
-			<Icon
-				id={icon}
-				size={20}
-				style={{
-					isolation: 'isolate',
-					display: 'inline-block',
-					marginBottom: '-3px',
-				}}
-			/>
-			<Input placeholder={placeholder} />
+			<Icon id={icon} size={iconSize} style={iconStyles} />
+			<Input placeholder={placeholder} size={size} width={width} />
+			<VisuallyHidden>{label}</VisuallyHidden>
 		</Wrapper>
 	);
 };
@@ -28,12 +29,26 @@ const Wrapper = styled.div`
 `;
 
 const Input = styled.input`
-	z-index: -1;
-	margin-left: -16px;
+	width: ${(p) => p.width}px;
 	border-top: none;
 	border-left: none;
 	border-right: none;
-	text-align: center;
+	border-bottom: ${(p) => (p.size === 'small' ? '1px' : '2px')} solid black;
+	font-family: 'Roboto', sans-serif;
+	font-size: ${(p) => (p.size === 'small' ? '.75rem' : '1rem')};
+	font-weight: 700;
+	text-indent: 8px;
+	color: ${COLORS.gray700};
+
+	&::placeholder {
+		text-indent: 8px;
+		font-weight: 400;
+		color: ${COLORS.gray500};
+	}
+
+	&:hover {
+		color: ${COLORS.black};
+	}
 `;
 
 export default IconInput;
