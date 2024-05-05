ac0b666ea4f1ec77f3da15e7bb2ab77b11475c55
Henrik Sundman <henriksundman@yahoo.se>
2024-05-05 13:00:10 +0200

    negative marigin

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,34 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+	return (
+		<Wrapper>
+			<Icon
+				id={icon}
+				size={20}
+				style={{
+					isolation: 'isolate',
+					display: 'inline-block',
+					marginBottom: '-3px',
+				}}
+			/>
+			<Input placeholder={placeholder} />
+		</Wrapper>
+	);
 };
+
+const Wrapper = styled.div`
+	max-width: fit-content;
+`;
+
+const Input = styled.input`
+	z-index: -1;
+	margin-left: -16px;
+	border-top: none;
+	border-left: none;
+	border-right: none;
+	text-align: center;
+`;
 
 export default IconInput;
