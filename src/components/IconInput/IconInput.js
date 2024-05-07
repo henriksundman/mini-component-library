16a21a447b6ad4f31d66cd83c5a7257693437b11
Henrik Sundman <henriksundman@yahoo.se>
2024-05-07 19:06:16 +0200

    add IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -7,41 +7,38 @@ import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
 const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
-	const iconStyles = {};
-	iconStyles.display = 'inline-block';
-	iconStyles.marginBottom = size === 'small' ? '-5.2px' : '-6.8px';
-	iconStyles.borderBottom =
-		size === 'small' ? '1px solid black' : '2px solid black';
-
 	const iconSize = size === 'small' ? 16 : 24;
+	const strokeWidth = size === 'small' ? 1 : 2;
 
 	return (
 		<Wrapper>
-			<Icon id={icon} size={iconSize} style={iconStyles} />
 			<Input placeholder={placeholder} size={size} width={width} />
+			<StyledIcon id={icon} size={iconSize} strokeWidth={strokeWidth} />
 			<VisuallyHidden>{label}</VisuallyHidden>
 		</Wrapper>
 	);
 };
 
 const Wrapper = styled.div`
+	position: relative;
 	max-width: fit-content;
 `;
 
 const Input = styled.input`
 	width: ${(p) => p.width}px;
+	height: ${(p) => (p.size === 'small' ? 20 : 26)}px;
 	border-top: none;
 	border-left: none;
 	border-right: none;
 	border-bottom: ${(p) => (p.size === 'small' ? '1px' : '2px')} solid black;
 	font-family: 'Roboto', sans-serif;
-	font-size: ${(p) => (p.size === 'small' ? '.75rem' : '1rem')};
+	font-size: ${(p) => (p.size === 'small' ? '1rem' : '1.25rem')};
 	font-weight: 700;
-	text-indent: 8px;
+	text-indent: ${(p) => (p.size === 'small' ? 24 : 36)}px;
 	color: ${COLORS.gray700};
+	cursor: pointer;
 
 	&::placeholder {
-		text-indent: 8px;
 		font-weight: 400;
 		color: ${COLORS.gray500};
 	}
@@ -49,6 +46,13 @@ const Input = styled.input`
 	&:hover {
 		color: ${COLORS.black};
 	}
+`;
+
+const StyledIcon = styled(Icon)`
+	pointer-events: none;
+	position: absolute;
+	bottom: 2px;
+	left: 2px;
 `;
 
 export default IconInput;
